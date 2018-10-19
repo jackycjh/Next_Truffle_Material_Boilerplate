3d534fd51f3d9a6dfc2a354ee5e5ec765de0cd0b
Jacky Chan <jacky.chan@chaintope.com>
2018-10-19 16:49:40 +0800

    Add Material-UI, with BottomNavigation.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '../components/Layout';
+import css from './index.css';
 
-const Index = () => {
-  return <Layout />
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const styles = {
+  // root: {
+  //   width: 500,
+  // },
+};
+
+class Index extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <Layout>
+        <BottomNavigation
+          value={value}
+          onChange={this.handleChange}
+          showLabels
+          className={css.nav}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Layout>
+    );
+  }
 }
 
-export default Index;
+export default withStyles(styles)(Index);
+// export default Index;
